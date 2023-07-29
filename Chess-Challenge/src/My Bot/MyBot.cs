fb790032a78ed960011da9f72acfda0850a08709
Selenaut <Selenaut@users.noreply.github.com>
2023-07-29 14:36:11 -0700

    - Added PeSTO eval and RFP. - Merged QSearch into Search. - Added comments explaning more complicated parts of the code.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,12 +5,36 @@ using ChessChallenge.API;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 public class MyBot : IChessBot
 {
                    //PieceType[] pieceTypes    = { PieceType.None, PieceType.Pawn, PieceType.Knight, PieceType.Bishop, PieceType.Rook, PieceType.Queen, PieceType.King};
-    private readonly       int[] k_pieceValues = {           0,              100,            300,              320,              500,            900,             20000 };
+    private readonly       int[] k_pieceValues = {           0,              100,            310,              330,              500,            900,             20000 };
 
+    int[] piecePhase = {0, 0, 1, 1, 2, 4, 0};
+    ulong[] psts = {
+    657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 
+    364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 
+    421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 
+    162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 
+    347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 
+    329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 
+    257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 
+    384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 
+    365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 
+    329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 
+    347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 
+    384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 
+    366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 
+    329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 
+    329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 
+    419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 
+    366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 
+    365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 
+    401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 
+    67159620133902};
+    
     private const byte INVALID = 0, EXACT = 1, LOWERBOUND = 2, UPPERBOUND = 3;
 
     //14 bytes per entry, likely will align to 16 bytes due to padding (if it aligns to 32, recalculate max TP table size)
@@ -27,7 +51,6 @@ public class MyBot : IChessBot
 
     Transposition[] m_TPTable;
     ulong k_TpMask = 0x7FFFFF; //4.7 million entries, likely consuming about 151 MB
-    sbyte k_maxDepth = 8;
     int k_timefraction = 40;
 
     Board m_board;
@@ -40,7 +63,7 @@ public class MyBot : IChessBot
 
     public MyBot()
     {
-        m_killerMoves = new Move[k_maxDepth * 2];
+        m_killerMoves = new Move[1024];
         m_TPTable = new Transposition[k_TpMask + 1];
     }
 
@@ -59,13 +82,13 @@ public class MyBot : IChessBot
         #endif
         Transposition bestMove = m_TPTable[board.ZobristKey & k_TpMask];
         int maxTime = timer.MillisecondsRemaining/k_timefraction;
-        for(sbyte depth = 1; depth <= k_maxDepth; depth++)
+        for(sbyte depth = 1;; depth++)
         {
             #if LOGGING
                 m_evals = 0;
                 m_nodes = 0;
             #endif
-            Search(depth, -100000000, 100000000, board.IsWhiteToMove ? 1 : -1);
+            Search(depth, -100000000, 100000000, board.IsWhiteToMove ? 1 : -1, true);
             bestMove = m_TPTable[board.ZobristKey & k_TpMask];
             #if LOGGING
                 Console.WriteLine("Depth: {0,2} | Nodes: {1,10} | Evals: {2,10} | Time: {3,5} Milliseconds | Best {4} | Eval: {5}", depth, m_nodes, m_evals, timer.MillisecondsElapsedThisTurn, bestMove.move, bestMove.evaluation);
@@ -79,99 +102,96 @@ public class MyBot : IChessBot
         return bestMove.move;
     }
 
-    int Search(int depth, int alpha, int beta, int color)
+    int Search(int depth, int alpha, int beta, int color, bool PVS)
     {
         #if LOGGING 
         m_nodes++;
         #endif
         
-        if(depth <= 0) return QSearch(depth, alpha, beta, color);
-        int bestEvaluation = int.MinValue;
+        bool inQSearch = depth <= 0;
+        int bestEvaluation = -2147483647;
         int startingAlpha = alpha;
 
+        //PVS stuff
+        //if(!PVS) beta = alpha--;
+
+        //See if we've checked this board state before
         ref Transposition transposition = ref m_TPTable[m_board.ZobristKey & k_TpMask];
         if(transposition.zobristHash == m_board.ZobristKey && transposition.flag != INVALID && transposition.depth >= depth)
         {
+            //If we have an "exact" score (a < score < beta) just use that
             if(transposition.flag == EXACT) return transposition.evaluation;
+            //If we have a lower bound better than beta, use that
             else if(transposition.flag == LOWERBOUND && transposition.evaluation >= beta)  return transposition.evaluation;
+            //If we have an upper bound worse than alpha, use that
             else if(transposition.flag == UPPERBOUND && transposition.evaluation <= alpha) return transposition.evaluation;
-            if(alpha >= beta) return transposition.evaluation;
         }
 
-        var moves = m_board.GetLegalMoves();
-
+        //Leaf node conditions
         if(m_board.IsDraw()) return -10;
         if(m_board.IsInCheckmate()) return m_board.PlyCount - 100000000;
 
+        int standingPat = Evaluate(color);
+
+        //RFP implementation
+        //Add PVS check when implemented properly
+        if(!m_board.IsInCheck() && !inQSearch && depth <= 3 && standingPat >= beta + 200*depth) return standingPat;
+
+        //QSearch: leaf none & standing-pat implementation
+        //Get all moves if in check or not in QSearch, otherwise get all moves
+        Move[] moves = m_board.GetLegalMoves(inQSearch && !m_board.IsInCheck());
+        if(moves.Length == 0) return standingPat; //Can't get here in checkmate (see above)
+        if(inQSearch)
+        {
+            if(standingPat >= beta) return beta;
+            if(standingPat > alpha) alpha = standingPat;
+        }
+
         OrderMoves(ref moves, depth);
  
+        //Assume first move is best move in position
         Move bestMove = moves[0];
 
-        foreach(Move m in moves)
+        for(int m = 0; m < moves.Length; m++)
         {
-            m_board.MakeMove(m);
-            int evaluation = -Search((sbyte)(depth - 1), -beta, -alpha, -color);
-            m_board.UndoMove(m);
+            m_board.MakeMove(moves[m]);
+            int evaluation = -Search((sbyte)(depth - 1), -beta, -alpha, -color, m == 0);
+            //PVS check;
+            //Force PVS if non-PV was good
+            //if(m != 0 && evaluation > alpha && evaluation < beta) evaluation = -Search((sbyte)(depth - 1), -beta, -alpha, -color, true);
+            m_board.UndoMove(moves[m]);
 
             if(bestEvaluation < evaluation)
             {
                 bestEvaluation = evaluation;
-                bestMove = m;
+                bestMove = moves[m];
             }
 
             alpha = Math.Max(alpha, bestEvaluation);
             if(alpha >= beta) break;
         }
 
-        //after finding best move
-        transposition.evaluation = bestEvaluation;
-        transposition.zobristHash = m_board.ZobristKey;
-        transposition.move = bestMove;
-        if(bestEvaluation < startingAlpha) 
-            transposition.flag = UPPERBOUND;
-        else if(bestEvaluation >= beta) 
+        //After finding best move from this board state,
+        //update TT with new best move;
+        //also mark if this is a "killer move"
+        if(!inQSearch)
         {
-            transposition.flag = LOWERBOUND;
-            if(!bestMove.IsCapture) 
-                m_killerMoves[depth] = bestMove;
+            transposition.evaluation = bestEvaluation;
+            transposition.zobristHash = m_board.ZobristKey;
+            transposition.move = bestMove;
+            if(bestEvaluation < startingAlpha) 
+                transposition.flag = UPPERBOUND;
+            else if(bestEvaluation >= beta) 
+            {
+                transposition.flag = LOWERBOUND;
+                if(!bestMove.IsCapture) 
+                    m_killerMoves[depth] = bestMove;
+            }
+            else transposition.flag = EXACT;
+            transposition.depth = (sbyte)depth;
         }
-        else transposition.flag = EXACT;
-        transposition.depth = (sbyte)depth;
 
         return bestEvaluation;
-    }
-
-    int QSearch(int depth, int alpha, int beta, int color)
-    {
-        #if LOGGING
-        m_nodes++;
-        #endif
-        
-        Move[] moves;
-        if(m_board.IsInCheck()) moves = m_board.GetLegalMoves();
-        else
-        {
-            moves = m_board.GetLegalMoves(true);
-            if(m_board.IsInCheckmate()) return -100000000 + m_board.PlyCount;
-            if(moves.Length == 0) return Evaluate(color);
-        }
-
-        alpha = Math.Max(Evaluate(color), alpha);
-        if(alpha >= beta) return beta;
-
-        OrderMoves(ref moves, depth);
- 
-        foreach(Move m in moves)
-        {
-            m_board.MakeMove(m);
-            int evaluation = -QSearch(depth - 1, -beta, -alpha, -color);
-            m_board.UndoMove(m);
-
-            alpha = Math.Max(evaluation, alpha);
-            if(alpha >= beta) break;
-        }
-
-        return alpha;
     }
 
     void OrderMoves(ref Move[] moves, int depth)
@@ -185,13 +205,17 @@ public class MyBot : IChessBot
     int GetMovePriority(Move move, int depth)
     {
         int priority = 0;
+        //a move in the TT is likely PV
         Transposition tp = m_TPTable[m_board.ZobristKey & k_TpMask];
         if(tp.move == move && tp.zobristHash == m_board.ZobristKey) 
             priority += 100000;
+        //Captures ordered in MVVLVA order
         else if (move.IsCapture) 
             priority =  1000 + 10 * (int)move.CapturePieceType - (int)move.MovePieceType;
+        //Check for a killer move
         else if (depth >= 0 && move.Equals(m_killerMoves[depth]))
             priority =  1;
+        //if none of the previous conditions passed priority = 0
         return priority;
     }
 
@@ -200,20 +224,38 @@ public class MyBot : IChessBot
         #if LOGGING
         m_evals++;
         #endif
-        int materialCount = 0;
-        for (int i = 0; ++i < 7;)
-        {
-            materialCount += (m_board.GetPieceList((PieceType)i, true).Count - m_board.GetPieceList((PieceType)i, false).Count) * k_pieceValues[i];
-        } 
-        var availableMoves = m_board.GetLegalMoves();
-        int mobility = availableMoves.Length;
-        return materialCount * color + mobility;
+
+        //Pulled from JW's example bot's implementation of compressed PeSTO (ComPresSTO?)
+
+        int mg = 0, eg = 0, phase = 0;
+
+        foreach(bool stm in new[] {true, false}) {
+            for(var p = PieceType.Pawn; p <= PieceType.King; p++) {
+                int piece = (int)p, ind;
+                ulong mask = m_board.GetPieceBitboard(p, stm);
+                while(mask != 0) {
+                    phase += piecePhase[piece];
+                    ind = 128 * (piece - 1) + BitboardHelper.ClearAndGetIndexOfLSB(ref mask) ^ (stm ? 56 : 0);
+                    mg += getPstVal(ind) + k_pieceValues[piece];
+                    eg += getPstVal(ind + 64) + k_pieceValues[piece];
+                }
+            }
+
+            mg = -mg;
+            eg = -eg;
+        }
+
+        return (mg * phase + eg * (24 - phase)) / 24 * color;
+    }
+
+    int getPstVal(int psq) {
+        return (int)(((psts[psq / 10] >> (6 * (psq % 10))) & 63) - 20) * 8;
     }
 
     bool ShouldExecuteNextDepth(Timer timer, int maxThinkTime)
     {
         int currentThinkTime = timer.MillisecondsElapsedThisTurn;
-        return ((maxThinkTime - currentThinkTime) > currentThinkTime * 3);
+        return ((maxThinkTime - currentThinkTime) > currentThinkTime * 2);
     }
 
 
