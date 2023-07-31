687668096b92428d6976d23bc210cf387c40553c
Selenaut <Selenaut@users.noreply.github.com>
2023-07-30 18:28:36 -0700

    Added UCI support for running via CuteChess/etc.

--- Chess-Challenge/src/Framework/Application/Core/Program.cs
+++ Chess-Challenge/src/Framework/Application/Core/Program.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Numerics;
 using System.Runtime.InteropServices;
+using System;
+using ChessChallenge.API;
+using ChessChallenge.UCI;
 
 namespace ChessChallenge.Application
 {
@@ -10,8 +13,31 @@ namespace ChessChallenge.Application
         const bool hideRaylibLogs = true;
         static Camera2D cam;
 
-        public static void Main()
+        public static void Main(string[] args)
         {
+            if(args.Length == 1 && args[0].Contains("cutechess"))
+            {
+                string argstr = args[0].Substring(args[0].IndexOf("uci"));
+                string[] ccArgs = argstr.Split(" ");
+                if(ccArgs.Length == 2 && ccArgs[0] == "uci")
+                {
+                    Console.WriteLine("Starting up in UCI mode...");
+                    StartUCI(ccArgs);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Improper CuteChess arg format; should be 'cutechess uci <botname>'");
+                    return;
+                }
+            }
+            if (args.Length > 1 && args[0] == "uci")
+            {
+                Console.WriteLine("Starting up in UCI mode...");
+                StartUCI(args);
+                return;
+            }
+            Console.WriteLine("Starting up in GUI mode...");
             Vector2 loadedWindowSize = GetSavedWindowSize();
             int screenWidth = (int)loadedWindowSize.X;
             int screenHeight = (int)loadedWindowSize.Y;
@@ -53,6 +79,27 @@ namespace ChessChallenge.Application
             UIHelper.Release();
         }
 
+        public static void StartUCI(string[] args)
+        {
+            bool success = Enum.TryParse(args[1], out ChallengeController.PlayerType player);
+
+            if (!success)
+            {
+                Console.Error.WriteLine($"Failed to start bot with player type {args[1]}");
+                return;
+            }
+
+            IChessBot? bot = ChallengeController.CreateBot(player);
+            if (bot == null)
+            {
+                Console.Error.WriteLine($"Cannot create bot of type {player.ToString()}");
+                return;
+            }
+
+            UCIBot uci = new UCIBot(bot, player);
+            uci.Run();
+        }
+        
         public static void SetWindowSize(Vector2 size)
         {
             Raylib.SetWindowSize((int)size.X, (int)size.Y);
